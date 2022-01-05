4049acc37fadf725e20f569b6f2f82bf7666b2d3
OpenGG <liy099@gmail.com>
2022-01-05 19:22:58 +0800

    fix(iconfont): PlanC

--- src/DemoIcons/PlanC.tsx
+++ src/DemoIcons/PlanC.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // 第一步：引入图标
 import '@ks/kw/icon/output/all/iconfont.css';
 import IconFont from '@ks/kw/icon/output/all/iconfont.js';
-import IconFontLight from '@ks/kw/icon/output/all/IconFontLight.js';
+import IconFontLight from '@ks/kw/icon/output/all/iconfontLight.js';
 
 // 第二步（可选）：引入颜色
 import '@ks/kwcolor/color/output/all/index.css';
